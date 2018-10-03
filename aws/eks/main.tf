9d0a1a7186961d9386658719ff3115cad107b625
Robert Bell <robert.bell@byton.com>
2018-10-03 11:03:10 -0700

    updated to more unique naming to match the repo

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 variable "cluster-name" {
-  default = "terraform-eks-demo"
+  default = "tf-eks-yeoldebrewery"
   type    = "string"
 }
 
@@ -11,58 +11,58 @@ variable "cluster-name" {
 # and can be swapped out as necessary.
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "demo" {
+resource "aws_vpc" "yeoldebrewery" {
   cidr_block = "10.0.0.0/16"
 
   tags = "${
     map(
-     "Name", "terraform-eks-demo-node",
+     "Name", "tf-eks-yeoldebrewery-node",
      "kubernetes.io/cluster/${var.cluster-name}", "shared",
     )
   }"
 }
 
-resource "aws_subnet" "demo" {
+resource "aws_subnet" "yeoldebrewery" {
   count = 2
 
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   cidr_block        = "10.0.${count.index}.0/24"
-  vpc_id            = "${aws_vpc.demo.id}"
+  vpc_id            = "${aws_vpc.yeoldebrewery.id}"
 
   tags = "${
     map(
-     "Name", "terraform-eks-demo-node",
+     "Name", "tf-eks-yeoldebrewery-node",
      "kubernetes.io/cluster/${var.cluster-name}", "shared",
     )
   }"
 }
 
-resource "aws_internet_gateway" "demo" {
-  vpc_id = "${aws_vpc.demo.id}"
+resource "aws_internet_gateway" "yeoldebrewery" {
+  vpc_id = "${aws_vpc.yeoldebrewery.id}"
 
   tags {
-    Name = "terraform-eks-demo"
+    Name = "tf-eks-yeoldebrewery"
   }
 }
 
-resource "aws_route_table" "demo" {
-  vpc_id = "${aws_vpc.demo.id}"
+resource "aws_route_table" "yeoldebrewery" {
+  vpc_id = "${aws_vpc.yeoldebrewery.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.demo.id}"
+    gateway_id = "${aws_internet_gateway.yeoldebrewery.id}"
   }
 }
 
-resource "aws_route_table_association" "demo" {
+resource "aws_route_table_association" "yeoldebrewery" {
   count = 2
 
-  subnet_id      = "${aws_subnet.demo.*.id[count.index]}"
-  route_table_id = "${aws_route_table.demo.id}"
+  subnet_id      = "${aws_subnet.yeoldebrewery.*.id[count.index]}"
+  route_table_id = "${aws_route_table.yeoldebrewery.id}"
 }
 
-resource "aws_iam_role" "demo-cluster" {
-  name = "terraform-eks-demo-cluster"
+resource "aws_iam_role" "yeoldebrewery-cluster" {
+  name = "tf-eks-yeoldebrewery-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -80,20 +80,20 @@ resource "aws_iam_role" "demo-cluster" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "yeoldebrewery-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.demo-cluster.name}"
+  role       = "${aws_iam_role.yeoldebrewery-cluster.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "yeoldebrewery-cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.demo-cluster.name}"
+  role       = "${aws_iam_role.yeoldebrewery-cluster.name}"
 }
 
-resource "aws_security_group" "demo-cluster" {
-  name        = "terraform-eks-demo-cluster"
+resource "aws_security_group" "yeoldebrewery-cluster" {
+  name        = "tf-eks-yeoldebrewery-cluster"
   description = "Cluster communication with worker nodes"
-  vpc_id      = "${aws_vpc.demo.id}"
+  vpc_id      = "${aws_vpc.yeoldebrewery.id}"
 
   egress {
     from_port   = 0
@@ -103,35 +103,35 @@ resource "aws_security_group" "demo-cluster" {
   }
 
   tags {
-    Name = "terraform-eks-demo"
+    Name = "tf-eks-yeoldebrewery"
   }
 }
 
 # OPTIONAL: Allow inbound traffic from your local workstation external IP
 #           to the Kubernetes. You will need to replace A.B.C.D below with
 #           your real IP. Services like icanhazip.com can help you find this.
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
+resource "aws_security_group_rule" "yeoldebrewery-cluster-ingress-workstation-https" {
   cidr_blocks       = ["67.207.104.155/32"]
   description       = "Allow workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.demo-cluster.id}"
+  security_group_id = "${aws_security_group.yeoldebrewery-cluster.id}"
   to_port           = 443
   type              = "ingress"
 }
 
-resource "aws_eks_cluster" "demo" {
+resource "aws_eks_cluster" "yeoldebrewery" {
   name            = "${var.cluster-name}"
-  role_arn        = "${aws_iam_role.demo-cluster.arn}"
+  role_arn        = "${aws_iam_role.yeoldebrewery-cluster.arn}"
 
   vpc_config {
-    security_group_ids = ["${aws_security_group.demo-cluster.id}"]
-    subnet_ids         = ["${aws_subnet.demo.*.id}"]
+    security_group_ids = ["${aws_security_group.yeoldebrewery-cluster.id}"]
+    subnet_ids         = ["${aws_subnet.yeoldebrewery.*.id}"]
   }
 
   depends_on = [
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
+    "aws_iam_role_policy_attachment.yeoldebrewery-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.yeoldebrewery-cluster-AmazonEKSServicePolicy",
   ]
 }
 
@@ -142,8 +142,8 @@ locals {
 apiVersion: v1
 clusters:
 - cluster:
-    server: ${aws_eks_cluster.demo.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.demo.certificate_authority.0.data}
+    server: ${aws_eks_cluster.yeoldebrewery.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.yeoldebrewery.certificate_authority.0.data}
   name: kubernetes
 contexts:
 - context:
@@ -170,8 +170,8 @@ output "kubeconfig" {
   value = "${local.kubeconfig}"
 }
 
-resource "aws_iam_role" "demo-node" {
-  name = "terraform-eks-demo-node"
+resource "aws_iam_role" "yeoldebrewery-node" {
+  name = "tf-eks-yeoldebrewery-node"
 
   assume_role_policy = <<POLICY
 {
@@ -189,30 +189,30 @@ resource "aws_iam_role" "demo-node" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "yeoldebrewery-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "${aws_iam_role.demo-node.name}"
+  role       = "${aws_iam_role.yeoldebrewery-node.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "yeoldebrewery-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = "${aws_iam_role.demo-node.name}"
+  role       = "${aws_iam_role.yeoldebrewery-node.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "yeoldebrewery-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = "${aws_iam_role.demo-node.name}"
+  role       = "${aws_iam_role.yeoldebrewery-node.name}"
 }
 
-resource "aws_iam_instance_profile" "demo-node" {
-  name = "terraform-eks-demo"
-  role = "${aws_iam_role.demo-node.name}"
+resource "aws_iam_instance_profile" "yeoldebrewery-node" {
+  name = "tf-eks-yeoldebrewery"
+  role = "${aws_iam_role.yeoldebrewery-node.name}"
 }
 
-resource "aws_security_group" "demo-node" {
-  name        = "terraform-eks-demo-node"
+resource "aws_security_group" "yeoldebrewery-node" {
+  name        = "tf-eks-yeoldebrewery-node"
   description = "Security group for all nodes in the cluster"
-  vpc_id      = "${aws_vpc.demo.id}"
+  vpc_id      = "${aws_vpc.yeoldebrewery.id}"
 
   egress {
     from_port   = 0
@@ -223,38 +223,38 @@ resource "aws_security_group" "demo-node" {
 
   tags = "${
     map(
-     "Name", "terraform-eks-demo-node",
+     "Name", "tf-eks-yeoldebrewery-node",
      "kubernetes.io/cluster/${var.cluster-name}", "owned",
     )
   }"
 }
 
-resource "aws_security_group_rule" "demo-node-ingress-self" {
+resource "aws_security_group_rule" "yeoldebrewery-node-ingress-self" {
   description              = "Allow node to communicate with each other"
   from_port                = 0
   protocol                 = "-1"
-  security_group_id        = "${aws_security_group.demo-node.id}"
-  source_security_group_id = "${aws_security_group.demo-node.id}"
+  security_group_id        = "${aws_security_group.yeoldebrewery-node.id}"
+  source_security_group_id = "${aws_security_group.yeoldebrewery-node.id}"
   to_port                  = 65535
   type                     = "ingress"
 }
 
-resource "aws_security_group_rule" "demo-node-ingress-cluster" {
+resource "aws_security_group_rule" "yeoldebrewery-node-ingress-cluster" {
   description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
   from_port                = 1025
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-node.id}"
-  source_security_group_id = "${aws_security_group.demo-cluster.id}"
+  security_group_id        = "${aws_security_group.yeoldebrewery-node.id}"
+  source_security_group_id = "${aws_security_group.yeoldebrewery-cluster.id}"
   to_port                  = 65535
   type                     = "ingress"
 }
 
-resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
+resource "aws_security_group_rule" "yeoldebrewery-cluster-ingress-node-https" {
   description              = "Allow pods to communicate with the cluster API Server"
   from_port                = 443
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-cluster.id}"
-  source_security_group_id = "${aws_security_group.demo-node.id}"
+  security_group_id        = "${aws_security_group.yeoldebrewery-cluster.id}"
+  source_security_group_id = "${aws_security_group.yeoldebrewery-node.id}"
   to_port                  = 443
   type                     = "ingress"
 }
@@ -279,38 +279,38 @@ data "aws_region" "current" {}
 # information into the AutoScaling Launch Configuration.
 # More information: https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html
 locals {
-  demo-node-userdata = <<USERDATA
+  yeoldebrewery-node-userdata = <<USERDATA
 #!/bin/bash
 set -o xtrace
-/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.demo.endpoint}' --b64-cluster-ca '${aws_eks_cluster.demo.certificate_authority.0.data}' '${var.cluster-name}'
+/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.yeoldebrewery.endpoint}' --b64-cluster-ca '${aws_eks_cluster.yeoldebrewery.certificate_authority.0.data}' '${var.cluster-name}'
 USERDATA
 }
 
-resource "aws_launch_configuration" "demo" {
+resource "aws_launch_configuration" "yeoldebrewery" {
   associate_public_ip_address = true
-  iam_instance_profile        = "${aws_iam_instance_profile.demo-node.name}"
+  iam_instance_profile        = "${aws_iam_instance_profile.yeoldebrewery-node.name}"
   image_id                    = "${data.aws_ami.eks-worker.id}"
   instance_type               = "m4.large"
-  name_prefix                 = "terraform-eks-demo"
-  security_groups             = ["${aws_security_group.demo-node.id}"]
-  user_data_base64            = "${base64encode(local.demo-node-userdata)}"
+  name_prefix                 = "tf-eks-yeoldebrewery"
+  security_groups             = ["${aws_security_group.yeoldebrewery-node.id}"]
+  user_data_base64            = "${base64encode(local.yeoldebrewery-node-userdata)}"
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_autoscaling_group" "demo" {
+resource "aws_autoscaling_group" "yeoldebrewery" {
   desired_capacity     = 2
-  launch_configuration = "${aws_launch_configuration.demo.id}"
+  launch_configuration = "${aws_launch_configuration.yeoldebrewery.id}"
   max_size             = 2
   min_size             = 1
-  name                 = "terraform-eks-demo"
-  vpc_zone_identifier  = ["${aws_subnet.demo.*.id}"]
+  name                 = "tf-eks-yeoldebrewery"
+  vpc_zone_identifier  = ["${aws_subnet.yeoldebrewery.*.id}"]
 
   tag {
     key                 = "Name"
-    value               = "terraform-eks-demo"
+    value               = "tf-eks-yeoldebrewery"
     propagate_at_launch = true
   }
 
@@ -332,7 +332,7 @@ metadata:
   namespace: kube-system
 data:
   mapRoles: |
-    - rolearn: ${aws_iam_role.demo-node.arn}
+    - rolearn: ${aws_iam_role.yeoldebrewery-node.arn}
       username: system:node:{{EC2PrivateDNSName}}
       groups:
         - system:bootstrappers
